4e59b00509fce507172936883bf3fe90573425f5
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-02-24 16:10:00 +0100

    added one more fetch but it needed a delay due to asynch, now both are below one button

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const getDeckBtn = document.getElementById("get-deck-btn")
+const getCardsBtn = document.getElementById("get-cards-btn")
+let deckId
 
-getDeckBtn.addEventListener("click", () => {
+
+getCardsBtn.addEventListener("click", () => {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
         console.log(data)
+        deckId = data.deck_id
+        console.log(deckId)
     })
+    setTimeout(()=> {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => console.log(data.cards))
+    },2000)
 })
