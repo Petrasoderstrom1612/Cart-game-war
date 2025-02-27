298f1ad832e78629ae2a5247b6670e3495b31b57
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-02-27 11:55:42 +0100

    changed img type to png

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ getCardsBtn.addEventListener("click", () => {
 
             let cardsHtml = ""
             data.cards.forEach((oneCard)=>{
-                cardsHtml += `<img src=${oneCard.images.svg} alt="${oneCard.value + " " + oneCard.suit}"/>`
+                cardsHtml += `<img src=${oneCard.images.png} alt="${oneCard.value + " " + oneCard.suit}"/>`
             })
             return cardsSection.innerHTML = cardsHtml
         })
