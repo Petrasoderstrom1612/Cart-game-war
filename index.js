737ef17d7e8fda29c38d5e9f6faf9d43582c8ee2
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-03-06 09:28:42 +0100

    added my own version of solution - looping over the array and company values in array to my vard. Instead of indexof inbuilt function

--- index.js
+++ index.js
@@ -30,13 +30,35 @@ getCardsBtn.addEventListener("click", () => {
 })
 
 const determineWinner = (card1, card2) => {
-    let card1index = cardValues.indexOf(card1)
+    let card1index = cardValues.indexOf(card1) //an inbuild function indexOf(the value you search for in the array)
     let card2index = cardValues.indexOf(card2)
     console.log(card1index + 2 , card2index + 2)
 
-    const result = card1index > card2index ? `${card1} wins` : 
-                    card1index < card2index ? `${card2} wins` :
+    const result = card1index > card2index ? `${card1} wins with value ${card1index + 2}` : 
+                    card1index < card2index ? `${card2} wins with value ${card2index + 2}` :
                     "It's a tie"
     
     return result
 }
+
+// const determineWinner = (card1, card2) => { //my solution
+//     let realValue1 = null
+//     let realValue2 = null
+
+//     for (let i =0; i < cardValues.length; i++){ //loop though the cardValues arr and find there the same cart as the one you picked from the pile
+//         if (cardValues[i] === card1){
+//             realValue1 = i +2  //assign it the index number (since first card is 2, add 2 to it so it is more understandable for human eye)
+//             console.log("real value 1",realValue1)
+//         }
+//         if (cardValues[i] === card2){
+//             realValue2 = i + 2
+//             console.log("real value 2",realValue2)
+//         }
+//     }
+
+//     const result = realValue1 > realValue2 ? `${card1} wins with value ${realValue1 + 2}` : 
+//                     realValue1 < realValue2 ? `${card2} wins with value ${realValue2 + 2}` :
+//                     "It's a tie"
+//     return result
+
+// }
