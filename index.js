f2fe3b25ddba8da4db942e710d2521cf8b5edaaa
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-03-11 08:36:12 +0100

    fixed the points being given per index

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const remaining = document.getElementById("remaining")
 const resultEl = document.getElementById("result-el")
 const card1El = document.getElementById("card1")
 const card2El = document.getElementById("card2")
-let card1 = 0
-let card2 = 0
+let computerScore = 0
+let myScore = 0
 let deckId
+let card1index
+let card2index
 
 
 
@@ -25,64 +27,79 @@ getCardsBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {console.log(data)
+
         remaining.textContent = `remaining cards: ${data.remaining}`
+
             let cardsHtml = ""
             data.cards.forEach((oneCard)=>{
                 cardsHtml += `<div class="card-slot"><img src=${oneCard.images.png} alt="${oneCard.value + " " + oneCard.suit}" class="card"/></div>`
                 // cardsContainer.children[0].innerHTML  /* If you would like to skip creating the extra div that is wrapping around the img
             })
              cardsSection.innerHTML = cardsHtml
-            
-             resultEl.innerHTML = determineWinner(data.cards[0].value,data.cards[1].value)
-
-             if(data.remaining === 0){
-                getCardsBtn.disabled = true;
+             
+             // ONE WAY TO DO IT
+             const cardValues = {
+                 2: 2,
+                 3: 3,
+                 4: 4,
+                 5: 5,
+                 6: 6,
+                 7: 7,
+                 8: 8,
+                 9: 9,
+                 10: 10,
+                 JACK: 11,
+                 QUEEN: 12,
+                 KING: 13,
+                 ACE: 14
              }
-        })
+             card1index = cardValues[data.cards[0].value] //when you want to get property value from an object based on key(property name)
+             card2index = cardValues[data.cards[1].value]
 
-        card1El.innerHTML = card1
-        card2El.innerHTML = card2
+             
+             resultEl.innerHTML = determineWinner(card1index,card2index)
+             
+             if(data.remaining === 0){
+                 getCardsBtn.disabled = true;
+                 if(computerScore > myScore){
+                     resultEl.innerHTML = `Computer wins ${computerScore} to ${myScore}`
+                    }
+                    else if(computerScore < myScore){
+                        resultEl.innerHTML = `You win ${myScore} to ${computerScore}`
+                    }
+                    else{
+                        "It's a tie 26 to 26"
+                    }
+                }
+            })
 })
 
-const determineWinner = (card1, card2) => {
-    // ONE  WAY TO DO IT
-    // const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
-    // let card1index = cardValues.indexOf(card1) //an inbuild function indexOf(the value you search for in the array)
-    // let card2index = cardValues.indexOf(card2)
 
-    // SECOND WAY TO DO IT
-    const cardValues = {
-        2: 2,
-        3: 3,
-        4: 4,
-        5: 5,
-        6: 6,
-        7: 7,
-        8: 8,
-        9: 9,
-        10: 10,
-        JACK: 11,
-        QUEEN: 12,
-        KING: 13,
-        ACE: 14
-    }
-    let card1index = cardValues[card1] //when you want to get property value from an object based on key(property name)
-    let card2index = cardValues[card2]
-
-    card1El.innerHTML = card1
-    card2El.innerHTML = card2
+const determineWinner = (card1index, card2index) => { 
     
     if (card1index > card2index) {
-        card1++
+        computerScore = computerScore + card1index
+        showPlayersScore(computerScore, myScore)
         return "Card 1 wins!"
     } else if (card1index < card2index) {
-        card2++
+        myScore = myScore + card2index
+        showPlayersScore(computerScore, myScore)
         return "Card 2 wins!"
     } else {
         return "War!"
     }
     
 }
+
+const showPlayersScore = (computerScore, myScore) =>{
+    card1El.innerHTML = `Computer score: ${computerScore}`
+    card2El.innerHTML = `My score: ${myScore}`
+}
+
+// SECOND  WAY TO DO IT
+// const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+// let card1index = cardValues.indexOf(card1) //an inbuild function indexOf(the value you search for in the array)
+// let card2index = cardValues.indexOf(card2)
 
 // THIRD WAY TO DO IT
 // const determineWinner = (card1, card2) => { //my solution
